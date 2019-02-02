1cd98a86a5cfd166fd58a8cb40a3043bfd599e06
Mengqi Wang <wmq516@gmail.com>
2019-02-01 23:04:11 -0700

    added initial version

--- index.js
+++ index.js
@@ -22,6 +22,30 @@ function fizzBuzz() {
     })
 }
 
-
-
 $(fizzBuzz)
+
+// Below is what we had by the end of 30 minutes
+function fizzBuzz() {
+    $('#number-chooser').submit(function(event) {
+      event.preventDefault();
+      let count = $('#number-choice').val();
+      let result = [];
+      for (let i = 0; i < count; i++) {
+        if ((i % 3 !== 0) && (i % 5 !== 0)) {
+          result.push(`
+           <div class="fizz-buzz-item">
+          <span>${i}</span>
+          </div>
+        `);
+        }
+        
+        }
+        console.log(result);
+      $('.js-results').append(result);
+      $('#number-choice').val(" ");  
+    } )
+    
+    
+    }
+    
+    $(fizzBuzz);
